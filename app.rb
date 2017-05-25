8f245143928d5c7d3dce177bd811ef88b75e6067
Christopher Lake <mr.lake@gmail.com>
2017-05-25 12:34:15 -0700

    WIP

--- app.rb
+++ app.rb
@@ -7,7 +7,15 @@ use Rack::Env
 enable :sessions
 
 helpers do
+  PLAYER1 = 'Player 1'
+  PLAYER2 = 'Player 2'
 
+  SWITCH_PLAYER = {
+    PLAYER2 => PLAYER1,
+    PLAYER1 => PLAYER2
+  }
+
+  # MOVE THIS LOGIC TO GAME CLASS
   def start_game
     session.clear
     game = session[:game] = Game.new
@@ -19,25 +27,33 @@ helpers do
   end
 
   # get the player's name when initializing 1Player vs Computer !?!?
-  def player
-    session[:player] ||= 'player1'
+  def current_player
+    session[:current_player] ||= PLAYER1
   end
 
   def switch_turns
-    session[:player] = player == 'player1' ? 'player2' : 'player1'
+    # TO DO
+    # player2 = num_players == 1 ? PLAYER2 : 'Computer'
+    session[:current_player] = SWITCH_PLAYER[current_player]
+  end
+
+  def num_players
+    session[:num_players]
   end
 end
 
+# TO DO redirect any 404s or 500s to root ('/')
+
 get '/' do
   start_game
-  @player = player
+  @current_player = current_player
   @board = session[:board]
 
   erb :index
 end
 
 post '/move' do
-  updated_board = game.update_board(session[:board], params[:slot].to_i, player)
+  updated_board = game.update_board(session[:board], params[:slot].to_i, current_player)
   session[:board] = updated_board
   switch_turns
 
@@ -46,9 +62,13 @@ end
 
 get '/in_progress' do
   @board = session[:board]
-  @player = player
+  @current_player = current_player
 
   erb :index
+end
+
+post '/set_players' do
+  nun_players = params[:num_players].to_i
 end
 
 post '/reset' do
--- models/game.rb
+++ models/game.rb
@@ -1,22 +1,25 @@
 class Game
   attr_accessor :current_board, :currrent_player, :current_checker, :checker_coordinates
 
+  # Add Board class back
+
   def initialize(rows: nil, columns: nil)
     @rows =  ENV['DEFAULT_BOARD_ROWS'].to_i
     @columns = ENV['DEFAULT_BOARD_COLUMNS'].to_i
   end
 
   def new_board
-    @columns.times.map { Array.new(@rows, '<>') }
+    @columns.times.map { Array.new(@rows, 0) }
   end
 
   def update_board(board, column, player)
-    current_board, currrent_player = board, player
-    current_checker = player == 'player1' ? 'O' : 'X'
-    checker_coordinates = drop_checker(column)
-    count = count_consecutive_checkers(checker_coordinates)
+    self.current_board, self.currrent_player = board, player
+    # use constant for player
+    self.current_checker = player == 'Player 1' ? 1 : -1
+    self.checker_coordinates = drop_checker(column)
+    count = (count_consecutive_checkers || 0)
 
-    if count > 3
+    if count > 3 || count < -3
       game_over_with_winner!
     else
       return current_board
@@ -32,14 +35,14 @@ class Game
   def drop_checker(column)
     # TODO check if board is full
     row = current_board.length - 1
-    until(current_board[row][column] == '<>') do
+    until(current_board[row][column] == 0) do
       row -= 1
     end
-    current_board[row][column] = checker
+    current_board[row][column] = current_checker
     [column, row]
   end
 
-  def count_consecutive_checkers(checker_coordinates)
+  def count_consecutive_checkers
     # code goes here
   end
 end
